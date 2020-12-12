aee5f32e76484b195f59ffa2a4efafc1c5dceae8
SHELL <shell.egor.jesus@gmail.com>
2020-12-13 00:03:24 +0600

    Edit JSON

---
+++ src/Components/ItemsInCart.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="basket-block__item__img-name">
+        <img src ="${this.img}" class="basket-block__item-img"></img>
+        <div class="basket-block__item-round"></div>
+        <div class="basket-block__item-naming">
+            <span class="basket-block__tag">${tag}</span> 
+            <span>${name}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
